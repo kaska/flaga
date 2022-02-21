91f2b0078fbcd37e85189af5c683171791d02fad
szemkelka@gmail.com <szemkelka@gmail.com>
2022-02-21 18:45:45 +0100

    wersja 2

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def index():
     text = open('xd.txt').read()
     return render_template("index.html", text=text)
 
+@app.route('/xd')
+def xd():
+    return render_template("xd.html")
+
 if __name__=="__main__":
     app.run()
